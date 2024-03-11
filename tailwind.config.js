9d05de5a6645fda6f91efaa80c53888c5135e079
unknown <jhonesdigno666@gmail.com>
2024-03-11 15:35:09 +0800

    FInished landing page with RWD

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./*.html"],
-	theme: {
-		screens: {
-			sm: "480px",
-			md: "768px",
-			lg: "976px",
-			xl: "1440px"
-		},
-		extend: {
-			colors: {
-				strongCyan: "hsl(171, 66%, 44%)",
-				lightBlue: "hsl(233, 100%, 69%)",
-				darkGrayishBlue: "hsl(210, 10%, 33%)",
-				grayishBlue: "hsl(201, 11%, 66%)"
-			},
-			fontFamily: {
-				sans: ["Bai Jamjuree", "sans-serif"]
-			}
-		}
-	},
-	plugins: []
+    content: ["./*.html"],
+    theme: {
+        container: {
+            center: true,
+        },
+        screens: {
+            sm: "480px",
+            md: "768px",
+            lg: "976px",
+            xl: "1440px",
+        },
+
+        extend: {
+            colors: {
+                strongCyan: "hsl(171, 66%, 44%)",
+                lightBlue: "hsl(233, 100%, 69%)",
+                darkGrayishBlue: "hsl(210, 10%, 33%)",
+                grayishBlue: "hsl(201, 11%, 66%)",
+                shadowStrongCyan: "rgba(38, 187, 164, .5)",
+                shadowLightBlue: "rgba(97, 115, 255, .5)",
+            },
+            fontFamily: {
+                sans: ["Bai Jamjuree", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
 };
